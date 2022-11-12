67b76b258620c848add6372e76d1671995cc0956
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2022-11-12 22:11:35 +0100

    refactor: updated grid plot

--- examples/plotting/grid_plot.py
+++ examples/plotting/grid_plot.py
@@ -9,7 +9,7 @@ from dynpssimrt.gui import LineOutageWidget, SimulationControl
 from dynpssimrt.time_window_plot import TimeWindowPlot
 from dynpssimrt.rtsim_plot import RTSimPlot, SyncPlot
 from dynpssimrt.plotting.phasor_plots import VoltagePhasorPlot
-from dynpssimrt.plotting.grid_plot import GridPlot3D
+from dynpssimrt.plotting.grid_plot import LiveGridPlot3D
 
 
 def main():
@@ -26,13 +26,14 @@ def main():
 
     sync_plot = SyncPlot(rts=rts, n_samples=1000, update_freq=25)
     voltage_phasor_plot = VoltagePhasorPlot(rts, update_freq=25)
-    grid_plot = GridPlot3D(rts=rts, z_ax='angle')
+    grid_plot = LiveGridPlot3D(rts=rts, z_ax='angle')
     # sync_plot_2 = SyncPlot(n_samples=1000, update_freq=10)
     time.sleep(1)
     rts.start()
     # grid_plot.start()
     sync_plot.start()
     voltage_phasor_plot.start()
+    grid_plot.start()
 
 
     # Add Control Widgets
--- src/dynpssimrt/plotting/grid_plot.py
+++ src/dynpssimrt/plotting/grid_plot.py
@@ -3,67 +3,50 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
 import pyqtgraph.opengl as gl
-# import dynpssimpy.dynamic as dps
-# import dynpssimpy.real_time_sim.apps as rts_apps
 import dynpssimpy.utility_functions as dps_uf
-# import importlib
-# from pyqtconsole.console import PythonConsole
-# import dynpssimpy.real_time_sim.sim as dps_rts
 import networkx as nx
 import time
+from dynpssimrt.interfacing import InterfacerQueuesThread
 
 
 class GridPlot3D(QtWidgets.QWidget):
-    def __init__(self, rts, update_freq=50, z_ax='abs_pu', use_colors=False, rotating=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.rts = rts
-        self.ps = rts.ps
-        self.dt = self.rts.dt
-        ps = self.ps
+    def __init__(
+        self,
+        bus_names,
+        n_lines,
+        n_trafos,
+        lines_from_to_y,
+        trafos_from_to_y,
+        from_buses_all,
+        to_buses_all,
+        offset_z,
+        scale=10,
+        update_freq=50,
+        z_ax='angle',
+        use_colors=False
+    ):
+        super().__init__()
         self.z_ax = z_ax
+        self.scale = scale
 
         # nx.draw(G)
-        self.scale = 10
+        self.n_bus = n_bus = len(bus_names)
+        
 
-        if self.z_ax == 'angle':
-            self.scale_z = 10*np.ones(ps.n_bus)
-            self.offset_z = 3  # *np.ones(ps.n_bus)
-        elif self.z_ax == 'abs':
-            self.scale_z = 10 * ps.v_n / max(ps.v_n) * 0.3
-            self.offset_z = 0  # np.zeros(ps.n_bus)
-
-        elif self.z_ax == 'abs_pu':
-            self.scale_z = 10 * 0.3*(ps.v_n**0)
-            self.offset_z = 0  # np.zeros(ps.n_bus)
-        # elif self.z_ax == 'both':
-        #     self.scale_z = 10*np.ones(ps.n_bus)
-        #     self.offset_z = 12*np.ones(ps.n_bus)
-
-
-        line_admittances = np.zeros(ps.lines['Line'].n_units, dtype=[('Y', float)])
-        line_admittances[:] = abs(ps.lines['Line'].admittance)
-        # for i, line in enumerate(ps.lines):
-            # line_admittances[i] = abs(ps.read_admittance_data('line', line)[2])
-
-        trafo_admittances = np.zeros(ps.trafos['Trafo'].n_units, dtype=[('Y', float)])
-        trafo_admittances[:] = abs(ps.trafos['Trafo'].admittance)
-        # for i, trafo in enumerate(ps.transformers):
-            # trafo_admittances[i] = abs(ps.read_admittance_data('transformer', trafo)[2])
 
         self.G = nx.MultiGraph()
-        self.G.add_nodes_from(ps.buses['name'])
+        self.G.add_nodes_from(bus_names)
         # G.add_edges_from(ps.lines[['from_bus', 'to_bus']])
         # G.add_edges_from(ps.transformers[['from_bus', 'to_bus']])
-        self.G.add_weighted_edges_from(
-            dps_uf.combine_recarrays(ps.lines['Line'].par, line_admittances)[['from_bus', 'to_bus', 'Y']])
-        self.G.add_weighted_edges_from(
-            dps_uf.combine_recarrays(ps.trafos['Trafo'].par, trafo_admittances)[['from_bus', 'to_bus', 'Y']])
 
+        self.G.add_weighted_edges_from(lines_from_to_y)
+        self.G.add_weighted_edges_from(trafos_from_to_y)
+        
         self.grid_layout()
 
-        self.n_edges = ps.lines['Line'].n_units + ps.trafos['Trafo'].n_units
-        self.edge_from_bus = np.concatenate([dps_uf.lookup_strings(type_.par['from_bus'], ps.buses['name']) for type_ in [ps.lines['Line'], ps.trafos['Trafo']]])
-        self.edge_to_bus = np.concatenate([dps_uf.lookup_strings(type_.par['to_bus'], ps.buses['name']) for type_ in [ps.lines['Line'], ps.trafos['Trafo']]])
+        self.n_edges = n_lines + n_trafos
+        self.edge_from_bus = from_buses_all
+        self.edge_to_bus = to_buses_all
 
         self.edge_x = np.vstack([self.x[self.edge_from_bus], self.x[self.edge_to_bus]]).T
         self.edge_y = np.vstack([self.y[self.edge_from_bus], self.y[self.edge_to_bus]]).T
@@ -78,15 +61,16 @@ class GridPlot3D(QtWidgets.QWidget):
         self.window.setCameraPosition(distance=30, elevation=12)
         self.window.show()
 
-        self.rotating = rotating
+        # self.rotating = rotating
 
         self.gz = gl.GLGridItem()
-        self.gz.translate(dx=0, dy=0, dz=-self.offset_z)
+        self.gz.translate(dx=0, dy=0, dz=-offset_z)
         self.window.addItem(self.gz)
 
-        color = np.ones((ps.n_bus, 4))
+        color = np.ones((self.n_bus, 4))
         color[:, -1] = 0.5
-        # if use_colors:
+        if use_colors:
+            color = np.array([self.colors(i).getRgb() for i in range(self.n_bus)]) / 255
             # color[ps.gen_bus_idx, :] = np.array([self.colors(i).getRgb() for i in range(self.ps.n_gen_bus)]) / 255
         # else:
             # color[ps.gen_bus_idx, :] = np.array([1 / 3, 2 / 3, 1, 0.5])[None, :]
@@ -117,25 +101,25 @@ class GridPlot3D(QtWidgets.QWidget):
 
         # self.graphWidget.show()
 
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.update)
-        self.timer.start(1000//update_freq)
-        self.t_prev = time.time()
+        # self.timer = QtCore.QTimer()
+        # self.timer.timeout.connect(self.update)
+        # self.timer.start(1000//self.update_freq)
+        # self.t_prev = time.time()
 
     def grid_layout(self, layout='spring_layout'):
-        ps = self.ps
+        # ps = self.ps
 
         if layout == 'spring_layout':
             pos = getattr(nx, layout)(self.G, seed=0)
         else:
             pos = getattr(nx, layout)(self.G)
 
-        x = np.zeros(ps.n_bus)
-        y = np.zeros(ps.n_bus)
+        x = np.zeros(self.n_bus)
+        y = np.zeros(self.n_bus)
         for i, key in enumerate(pos.keys()):
             x[i], y[i] = self.scale * pos[key]
 
-        z = self.scale_z * 1
+        z = np.ones(self.n_bus) * 1
 
         self.x = x
         self.y = y
@@ -144,30 +128,18 @@ class GridPlot3D(QtWidgets.QWidget):
         self.y0 = y.copy()
         self.z0 = z.copy()
 
-    def update(self):
-        if self.rotating:
-            x, y, z = self.window.cameraPosition()
-            t_now = time.time()
-            dt = t_now - self.t_prev
-            self.t_prev = t_now
-            new_angle = np.arctan2(y, x) * 180 / np.pi + 0.01*360*dt
-            self.window.setCameraPosition(azimuth=new_angle)
+    def update(self, z):
 
-        ps = self.rts.ps
+        # if self.rotating:
+        #     x, y, z = self.window.cameraPosition()
+        #     t_now = time.time()
+        #     dt = t_now - self.t_prev
+        #     self.t_prev = t_now
+        #     new_angle = np.arctan2(y, x) * 180 / np.pi + 0.01*360*dt
+        #     self.window.setCameraPosition(azimuth=new_angle)
 
-        v = ps.red_to_full.dot(self.rts.sol.v)
-        if self.z_ax == 'angle':
-            gen_mean_angle = np.mean(np.unwrap(self.rts.x[ps.gen['GEN'].state_idx_global['angle']]))
-            v_angle = np.angle(v) - gen_mean_angle
-            v_angle = (v_angle + np.pi) % (2*np.pi) - np.pi
-            v_angle -= np.mean(v_angle)
-            k = v_angle
-            self.z = self.scale_z*k + self.offset_z
-        elif self.z_ax in ['abs', 'abs_pu']:
-            k = abs(v)
-            self.z = self.scale_z*k + self.offset_z
 
-        # Branch currents
+          # Branch currents
         # flows = abs(np.concatenate([ps.v_to_i_lines.dot(v), ps.v_to_i_trafos.dot(v)]))
 
         # elif self.z_ax == 'both':
@@ -184,6 +156,9 @@ class GridPlot3D(QtWidgets.QWidget):
         # dy = amp*np.sin(v_angle)
         # self.x = self.x0 + dx
         # self.y = self.y0 + dy
+        # self.x = x
+        # self.y = y
+        self.z = z
 
         self.edge_x = np.vstack([self.x[self.edge_from_bus], self.x[self.edge_to_bus]]).T
         self.edge_y = np.vstack([self.y[self.edge_from_bus], self.y[self.edge_to_bus]]).T
@@ -196,3 +171,97 @@ class GridPlot3D(QtWidgets.QWidget):
 
         self.points.setData(pos=np.vstack([self.x, self.y, self.z]).T)
         self.lines.setData(pos=edge_pos)
+
+
+class LiveGridPlot3D(InterfacerQueuesThread):
+    def __init__(self, rts=None, update_freq=50, z_ax='angle', use_colors=False, *args, **kwargs):
+        self.z_ax = z_ax
+        InterfacerQueuesThread.__init__(self, rts, fs=update_freq)
+        
+        
+    @staticmethod
+    def get_init_data(rts):
+        ps = rts.ps
+        bus_names = ps.buses['name']
+        line_par = ps.lines['Line'].par
+        trafo_par = ps.trafos['Trafo'].par
+        line_admittances_ = ps.lines['Line'].admittance
+        trafo_admittances_ = ps.trafos['Trafo'].admittance
+        # v_n = ps.
+        return bus_names, line_par, trafo_par, line_admittances_, trafo_admittances_ 
+
+    def initialize(self, init_data):
+        bus_names, line_par, trafo_par, line_admittances_, trafo_admittances_  = init_data
+        self.n_bus = len(bus_names)
+        # v_n = 
+        n_lines = len(line_par)
+        n_trafos = len(trafo_par)
+
+        if self.z_ax == 'angle':
+            self.scale_z = 10*np.ones(self.n_bus)
+            self.offset_z = 3  # *np.ones(ps.n_bus)
+        # elif self.z_ax == 'abs':
+            # self.scale_z = 10 * v_n / max(v_n) * 0.3
+            # self.offset_z = 0  # np.zeros(ps.n_bus)
+
+        elif self.z_ax == 'abs_pu':
+            self.scale_z = 10 * 0.3*np.ones(self.n_bus)
+            self.offset_z = 0  # np.zeros(ps.n_bus)
+        # elif self.z_ax == 'both':
+        #     self.scale_z = 10*np.ones(ps.n_bus)
+        #     self.offset_z = 12*np.ones(ps.n_bus)
+        
+        line_admittances = np.zeros(n_lines, dtype=[('Y', float)])
+        line_admittances[:] = abs(line_admittances_)
+        
+        trafo_admittances = np.zeros(n_trafos, dtype=[('Y', float)])
+        trafo_admittances[:] = abs(trafo_admittances_)
+        
+        lines_from_to_y = dps_uf.combine_recarrays(
+            line_par,
+            line_admittances
+        )[['from_bus', 'to_bus', 'Y']]
+
+        trafos_from_to_y = dps_uf.combine_recarrays(
+            trafo_par,
+            trafo_admittances
+        )[['from_bus', 'to_bus', 'Y']]
+
+        from_buses_all = np.concatenate(
+            [dps_uf.lookup_strings(type_par['from_bus'], bus_names) for type_par in [line_par, trafo_par]])
+        to_buses_all = np.concatenate(
+            [dps_uf.lookup_strings(type_par['to_bus'], bus_names) for type_par in [line_par, trafo_par]])
+
+        self.grid_plot = GridPlot3D(
+            bus_names,
+            n_lines,
+            n_trafos,
+            lines_from_to_y,
+            trafos_from_to_y,
+            from_buses_all,
+            to_buses_all,
+            self.offset_z,
+        )
+
+    @staticmethod
+    def read_input_signal(rts):
+        voltages = rts.ps.red_to_full.dot(rts.sol.v)
+        gen_angles = rts.sol.x[rts.ps.gen['GEN'].state_idx_global['speed']]
+        return voltages, gen_angles
+    
+    def update(self, input):
+        voltages, gen_angles = input
+        
+        v = voltages
+        if self.z_ax == 'angle':
+            gen_mean_angle = np.mean(np.unwrap(gen_angles))
+            v_angle = np.angle(v) - gen_mean_angle
+            v_angle = (v_angle + np.pi) % (2*np.pi) - np.pi
+            v_angle -= np.mean(v_angle)
+            k = v_angle
+            self.z = self.scale_z*k + self.offset_z
+        elif self.z_ax in ['abs', 'abs_pu']:
+            k = abs(v)
+            self.z = self.scale_z*k + self.offset_z
+
+        self.grid_plot.update(self.z)
